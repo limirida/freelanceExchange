916a8a6c553d75f85468d7267639ef27b5f3ca93
Limirida <bloodsky2312@gmail.com>
2019-12-17 00:40:33 +0300

    add script

---
+++ script/script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const customer = document.getElementById('customer');
+    const freelancer = document.getElementById('freelancer');
+    const blockCustomer = document.getElementById('block-customer');
+    const blockFreelancer = document.getElementById('block-freelancer');
+    const blockChoice = document.getElementById('block-choice');
+    const btnExit = document.getElementById('btn-exit');
+    const formCustomer = document.getElementById('form-customer');
+    const orders = [];
+
+    customer.addEventListener('click', () => {
+        blockCustomer.style.display = 'block';
+        blockChoice.style.display = 'none';
+        btnExit.style.display = 'block';
+    });
+
+
+    freelancer.addEventListener('click', () => {
+        blockFreelancer.style.display = 'block';
+        blockChoice.style.display = 'none';
+        btnExit.style.display = 'block';
+    });
+
+    btnExit.addEventListener('click', () => {
+        btnExit.style.display = 'none';
+        blockCustomer.style.display = 'none';
+        blockFreelancer.style.display = 'none';
+        blockChoice.style.display = 'block';
+    })
+
+    formCustomer.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const obj = {};
+
+        for (const elem of formCustomer.elements) {
+            if ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+                (elem.type === 'radio' && elem.checked) ||
+                elem.tagName === "TEXTAREA") {
+                console.dir(elem)
+                obj[elem.name] = elem.value;
+
+
+                if (elem.type !== 'radio') {
+                    elem.value = '';
+                }
+            }
+        }
+        orders.push(obj);
+        console.log(orders);
+    });
+})
