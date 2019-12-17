246f7f21bef2ff2333f1c829aff5f48bcd9fe1d6
Limirida <bloodsky2312@gmail.com>
2019-12-17 22:45:40 +0300

    add script for table and modal

--- script/script.js
+++ script/script.js
@@ -6,7 +6,63 @@ document.addEventListener('DOMContentLoaded', () => {
     const blockChoice = document.getElementById('block-choice');
     const btnExit = document.getElementById('btn-exit');
     const formCustomer = document.getElementById('form-customer');
+    const ordersTable = document.getElementById('orders');
+    const modalOrder = document.getElementById('order_read');
+    const modalOrderActive = document.getElementById('order_active');
+
+
     const orders = [];
+
+    const renderOrders = () => {
+        ordersTable.textContent = '';
+
+        orders.forEach((order, i) => {
+            console.log(order);
+            ordersTable.innerHTML += `
+    <tr class="order" data-number-order="${i}">
+    <td>${i+1}</td>
+    <td>${order.title}</td>
+    <td class="${order.currency}"></td>
+    <td>${order.deadline}</td>
+    </tr>`;
+        });
+
+    }
+
+
+    const openModal = (numberOrder) => {
+        const order = orders[numberOrder];
+        const modal = order.active ? modalOrderActive : modalOrder;
+
+        const firstNameBlock = document.querySelector('.firstName');
+        const titleBlock = document.querySelector('.modal-title');
+        const emailBlock = document.querySelector('.email');
+        const descriptionBlock = document.querySelector('.description');
+        const currencyBlock = document.querySelector('.currency_img');
+        const countBlock = document.querySelector('.count');
+        const phoneBlock = document.querySelector('.phone');
+
+
+        titleBlock.textContent = order.title;
+        firstNameBlock.textContent = order.firstName;
+        emailBlock.textContent = order.email;
+        descriptionBlock.textContent = order.description;
+        currencyBlock.textContent = order.currency_img;
+        countBlock.textContent = order.count;
+        phoneBlock.textContent = order.phone;
+
+        modal.style.display = 'block';
+    };
+
+    ordersTable.addEventListener('click', (event) => {
+        const target = event.target;
+        console.log('target:', target);
+
+        const targetOrder = target.closest('.order')
+        if (targetOrder) {
+            openModal(targetOrder.dataset.numberOrder);
+        }
+    });
 
     customer.addEventListener('click', () => {
         blockCustomer.style.display = 'block';
@@ -17,6 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     freelancer.addEventListener('click', () => {
         blockFreelancer.style.display = 'block';
+        renderOrders();
         blockChoice.style.display = 'none';
         btnExit.style.display = 'block';
     });
@@ -28,25 +85,26 @@ document.addEventListener('DOMContentLoaded', () => {
         blockChoice.style.display = 'block';
     })
 
-    formCustomer.addEventListener('submit', (event) => {
-        event.preventDefault();
+    formCustomer.addEventListener('submit', (e) => {
+        e.preventDefault();
 
         const obj = {};
 
-        for (const elem of formCustomer.elements) {
-            if ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+        const elements = [...formCustomer.elements]
+            .filter((elem) => (elem.tagName === 'INPUT' && elem.type !== 'radio') ||
                 (elem.type === 'radio' && elem.checked) ||
-                elem.tagName === "TEXTAREA") {
-                console.dir(elem)
-                obj[elem.name] = elem.value;
+                elem.tagName === "TEXTAREA");
 
+        elements.forEach((elem) => {
+            obj[elem.name] = elem.value;
+            console.log(obj);
+        });
 
-                if (elem.type !== 'radio') {
-                    elem.value = '';
-                }
-            }
-        }
+        formCustomer.reset();
+
         orders.push(obj);
         console.log(orders);
     });
-})
+
+
+});
